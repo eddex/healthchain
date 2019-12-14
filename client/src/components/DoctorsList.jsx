0180d7753d1610759d03bdb6e3d49d2bc49f4b22
eddex <eddex@outlook.com>
2019-12-14 15:18:18 +0100

    revoke acces from doctor

--- client/src/components/DoctorsList.jsx
+++ client/src/components/DoctorsList.jsx
@@ -7,6 +7,15 @@ const giveAccessToDoctor = async (doctorAddress, contract, accounts, accountId) 
   console.log(permissions)
 }
 
+const revokeAccessFromDoctor = async (doctorAddress, contract, accounts, accountId) => {
+  const doctorsPermissions = await contract.methods.getDoctorsPermissions(doctorAddress).call({ from: accounts[accountId], gas: 100000 })
+  doctorsPermissions.map(async (address, index) => {
+    if (address === accounts[accountId]) {
+      await contract.methods.revokeAccessFromDoctor(doctorAddress, index).send({ from: accounts[accountId], gas: 100000 })
+    }
+  })
+}
+
 const DoctorsList = ({ items: doctors, contract, accounts, accountId }) => {
   return (
     <div>
@@ -17,12 +26,17 @@ const DoctorsList = ({ items: doctors, contract, accounts, accountId }) => {
             <div className='col-lg m-1'>{doctor.address}</div>
             <div className='col-lg-2 m-1'>
               <button className='btn btn-success'
-                onClick={() => giveAccessToDoctor(doctor.address, contract, accounts, accountId)}>
-                  Give Access
+                onClick={() => giveAccessToDoctor(doctor.address, contract, accounts, accountId)}
+                >
+                Give Access
               </button>
             </div>
             <div className='col-lg-2 m-1'>
-              <button disabled className='btn btn-warning'>Revoke Access</button>
+              <button className='btn btn-warning'
+                onClick={() => revokeAccessFromDoctor(doctor.address, contract, accounts, accountId)}
+                >
+                Revoke Access
+              </button>
             </div>
           </div>
         })}
