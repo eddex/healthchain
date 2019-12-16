77e34713c6dee3bfb79411baeeee5f7bc511aa5f
eddex <eddex@outlook.com>
2019-12-16 16:59:50 +0100

    reenable CORS

--- server/server.js
+++ server/server.js
@@ -18,8 +18,7 @@ var corsOptions = {
     if (whitelist.indexOf(origin) !== -1) {
       callback(null, true)
     } else {
-      callback(null, true)
-      //callback(new Error('Not allowed by CORS'))
+      callback(new Error('Not allowed by CORS'))
     }
   }
 }
